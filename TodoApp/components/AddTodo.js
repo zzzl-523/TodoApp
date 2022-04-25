26608721337d6bd4a7237582be68dbe15f795aac
Jihyun Lee <zzh23@naver.com>
2022-04-25 21:07:57 +0900

    textInput

--- TodoApp/components/AddTodo.js
+++ TodoApp/components/AddTodo.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, TextInput} from 'react-native';
 
 function AddTodo() {
-    return <View style={styles.block} />;
+    return (
+        <View style={styles.block}>
+            <TextInput placeholder='할 일을 입력하세요.' style={styles.input} />            
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
     block: {
         height: 64,
-        backgroundColor: 'red',
+        paddingHorizontal: 16,
+        borderColor: '#bdbdbd',
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        justifyContent:'center',
+    },
+    input: {
+        fontSize: 16,
+        paddingVertical: 8,
     },
 });
 
