713764199edfd247d42ac76587c7523f8ed2f7cc
Jihyun Lee <zzh23@naver.com>
2022-04-27 23:03:59 +0900

    keyboard

--- TodoApp/components/AddTodo.js
+++ TodoApp/components/AddTodo.js
@@ -1,8 +1,21 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, TextInput, Image} from 'react-native';
+import {View, StyleSheet, TextInput, Image, TouchableOpacity, Platform, TouchableNativeFeedback, Keyboard} from 'react-native';
 
 function AddTodo() {
     const [text, setText] = useState('');
+
+    const onPress = () => {
+        setText('');
+        Keyboard.dismiss();
+    };
+
+    const button = (
+        <View style={styles.buttonStyle}>
+            <Image 
+                source={require('../assets/icons/add_white/add_white.png')} 
+            />
+        </View>
+    );
 
     console.log(text);
 
@@ -13,24 +26,34 @@ function AddTodo() {
                 style={styles.input}
                 value={text}
                 onChangeText={setText}
+                onSubmitEditing={onPress}
+                returnKeyType="done"
             />
-            <View style={styles.buttonStyle}>
-                <Image source={require('../assets/icons/add_white/add_white.png')} />
-            </View>            
+            {Platform.OS === 'ios' ? 
+              <TouchableOpacity activeOpacity={0.5} onPress={onPress}>{button}</TouchableOpacity>
+              :
+                <View style={styles.circleWrapper}>
+                    <TouchableNativeFeedback onPress={onPress}>{button}</TouchableNativeFeedback>
+                </View>              
+            }
+                  
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     block: {
+        backgroundColor: 'white',
         height: 64,
         paddingHorizontal: 16,
         borderColor: '#bdbdbd',
         borderTopWidth: 1,
         borderBottomWidth: 1,
-        justifyContent:'center',
+        alignItems: 'center',
+        flexDirection: 'row',
     },
     input: {
+        flex: 1,
         fontSize: 16,
         paddingVertical: 8,
     },
@@ -42,6 +65,10 @@ const styles = StyleSheet.create({
         backgroundColor: '#26a69a',
         borderRadius: 24,
     },
+    circleWrapper: {
+        overflow: 'hidden',
+        borderRadius: 24,
+    }
 });
 
 export default AddTodo;
